ea8b3694c21bf50e3285bc8522b9c74a20241a28
Tehillah Kangamba <tehillahkangamba@gmail.com>
2021-03-19 14:29:57 -0500

    created UI for News feed

---
+++ app/src/main/java/comp3350/winSport/presentation/NewsFeedAdapter.java
@@ -0,0 +1,24 @@
+package comp3350.winSport.presentation;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NewsFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
