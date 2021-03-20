bcaf6c7ee9ff9ed8d85513fb94ec72769c11c56c
Tehillah Kangamba <tehillahkangamba@gmail.com>
2021-03-20 09:56:41 -0500

    created adapter viewholder stuff

--- app/src/main/java/comp3350/winSport/presentation/NewsFeedAdapter.java
+++ app/src/main/java/comp3350/winSport/presentation/NewsFeedAdapter.java
@@ -1,5 +1,6 @@
 package comp3350.winSport.presentation;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -10,17 +11,20 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import comp3350.winSport.R;
 import comp3350.winSport.objects.NewsPost;
-public class NewsFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class NewsFeedAdapter extends RecyclerView.Adapter <NewsFeedAdapter.NewsFeedHolder> {
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public NewsFeedHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_feed_card, parent, false);
+        return new NewsFeedAdapter.NewsFeedHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull NewsFeedHolder holder, int position) {
 
     }
+
+
 
     @Override
     public int getItemCount() {
@@ -36,7 +40,10 @@ public class NewsFeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
         public  NewsFeedHolder(@NonNull View itemView) {
             super(itemView);
-
+            photo=itemView.findViewById(R.id.post_image);
+            postTitle=itemView.findViewById(R.id.post_title);
+            caption=itemView.findViewById(R.id.caption);
+            datePosted=itemView.findViewById(R.id.date_posted);
         }
 
 
