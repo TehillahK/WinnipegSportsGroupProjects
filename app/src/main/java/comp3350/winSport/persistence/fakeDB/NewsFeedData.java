ecaa79ed266cd40eb53c8cbeb1decd736ccc5c1e
TehillahK <56933368+TehillahK@users.noreply.github.com>
2021-03-22 09:43:27 -0600

    added interface for news feed db and fake db data

--- app/src/main/java/comp3350/winSport/persistence/fakeDB/NewsFeedData.java
+++ app/src/main/java/comp3350/winSport/persistence/fakeDB/NewsFeedData.java
@@ -1,4 +1,36 @@
 package comp3350.winSport.persistence.fakeDB;
 
-public class NewsFeedData {
+import java.util.ArrayList;
+import java.util.List;
+
+import comp3350.winSport.objects.NewsPost;
+import comp3350.winSport.persistence.INewsFeed;
+
+public class NewsFeedData implements INewsFeed {
+    private List<NewsPost> newsPosts;
+    public NewsFeedData()
+    {
+        newsPosts=new ArrayList<>();
+        initData();
+    }
+    public  void  initData()
+    {
+        NewsPost post1= new NewsPost("Jets Trade deadline","22/03/21",
+                "Is cheveldayoff willing to spend on big target",0,0);
+        NewsPost post2= new NewsPost("Jets top guns fire blanks","20/03/21",
+                "Jets lose to Edmonton Oliers",0,0);
+        NewsPost post3= new NewsPost("Its awards season for Cole Caufield","21/03/21",
+                "Caufied become the first unanimous winner of big the ten player of the year's award"
+                ,0,0);
+        newsPosts.add(post2);
+        newsPosts.add(post3);
+        newsPosts.add(post1);
+
+    }
+
+
+    @Override
+    public List<NewsPost> getNewsPosts() {
+        return newsPosts;
+    }
 }
