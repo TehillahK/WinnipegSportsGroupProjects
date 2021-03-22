2aee30dd114405f11f4a256b6cb5c76897e24ee6
TehillahK <56933368+TehillahK@users.noreply.github.com>
2021-03-22 13:27:37 -0600

    finished real db classes

--- app/src/main/java/comp3350/winSport/persistence/INewsFeed.java
+++ app/src/main/java/comp3350/winSport/persistence/INewsFeed.java
@@ -3,6 +3,7 @@ package comp3350.winSport.persistence;
 import java.util.List;
 
 import comp3350.winSport.objects.NewsPost;
+import comp3350.winSport.objects.exceptions.InvalidNameException;
 
 public interface INewsFeed {
     List<NewsPost> getNewsPosts();
--- app/src/main/java/comp3350/winSport/persistence/hsqldb/NewsFeedDataHSQLDB.java
+++ app/src/main/java/comp3350/winSport/persistence/hsqldb/NewsFeedDataHSQLDB.java
@@ -2,11 +2,18 @@ package comp3350.winSport.persistence.hsqldb;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import comp3350.winSport.objects.NewsPost;
+
+import comp3350.winSport.objects.PlayerStatistic;
+import comp3350.winSport.objects.Team;
+import comp3350.winSport.objects.exceptions.InvalidNameException;
 import comp3350.winSport.persistence.INewsFeed;
 
 public class NewsFeedDataHSQLDB implements INewsFeed {
@@ -21,11 +28,41 @@ public class NewsFeedDataHSQLDB implements INewsFeed {
 
     private NewsPost fromResultSet(final ResultSet rs) throws SQLException
     {
-        return null;
+        final String title = rs.getString("TITLE");
+       // final int psGamesPlayed = rs.getInt("GamesPlayed");
+        final String datePosted= rs.getString("DATEPOSTED");
+        final String caption = rs.getString("CAPTION");
+
+        final int numLikes = rs.getInt("NUMLIKES");
+        final int image = rs.getInt("IMAGE");
+
+
+        return new NewsPost(title,datePosted,caption,numLikes,image);
+
+
     }
 
     @Override
-    public List<NewsPost> getNewsPosts() {
-        return null;
+    public List<NewsPost> getNewsPosts()  throws InvalidNameException  {
+        List<NewsPost> newsPosts;
+        newsPosts=new ArrayList<>();
+        try(final Connection c=connection())
+        {
+            final Statement st = c.createStatement();
+            final ResultSet rs = st.executeQuery("SELECT DISTINCT * FROM NEWS_FEED");
+
+            while (rs.next()) {
+                final NewsPost newsPost = fromResultSet(rs);
+                newsPosts.add(newsPost);
+            }
+
+            rs.close();
+            st.close();
+
+        }catch (final SQLException e) {
+            throw new PersistenceException(e);
+        }
+        return newsPosts;
+   //  return null;
     }
 }
