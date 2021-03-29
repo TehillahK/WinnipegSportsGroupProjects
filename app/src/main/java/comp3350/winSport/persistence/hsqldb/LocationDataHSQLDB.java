273df281ab0be05b8e232aa3a0e8d8d6136c57c2
School <waughc@myumanitoba.ca>
2021-03-29 08:11:08 -0500

    starting to add HSQLDB support, unfinished LocationDataHSQLDB class.

--- app/src/main/java/comp3350/winSport/persistence/hsqldb/LocationDataHSQLDB.java
+++ app/src/main/java/comp3350/winSport/persistence/hsqldb/LocationDataHSQLDB.java
@@ -1,4 +1,38 @@
 package comp3350.winSport.persistence.hsqldb;
 
-public class LocationDataHSQLDB {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+import comp3350.winSport.objects.Location;
+import comp3350.winSport.persistence.ILocation;
+
+public class LocationDataHSQLDB implements ILocation {
+
+    private final String dbPath;
+
+    public LocationDataHSQLDB(final String dbPath) {
+        this.dbPath = dbPath;
+    }
+
+    private Connection connection() throws SQLException {
+        return DriverManager.getConnection("jdbc:hsqldb:file:" + dbPath + ";shutdown=true", "SA", "");
+    }
+
+    @Override
+    public List<Location> getSportVenues() {
+
+
+
+        return null;
+    }
+
+    @Override
+    public List<Location> getNearestBars() {
+
+
+
+        return null;
+    }
 }
