3e0570fdd6042fc001b44e5128cb2611192501f4
TehillahK <56933368+TehillahK@users.noreply.github.com>
2021-03-29 12:50:45 -0600

    connected bars to db

--- app/src/main/java/comp3350/winSport/presentation/BarsActivity.java
+++ app/src/main/java/comp3350/winSport/presentation/BarsActivity.java
@@ -2,14 +2,24 @@ package comp3350.winSport.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import comp3350.winSport.R;
 import comp3350.winSport.business.AccessLocations;
+import comp3350.winSport.objects.Location;
+import comp3350.winSport.objects.exceptions.InvalidNameException;
+import comp3350.winSport.presentation.Adapters.NearestBarAdapter;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class BarsActivity extends AppCompatActivity {
-    AccessLocations bars;
+    AccessLocations accessBars;
+    NearestBarAdapter adapter;
+    NearestBarAdapter.RecyclerViewEventListener listener;
+    List<Location> bars;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +33,20 @@ public class BarsActivity extends AppCompatActivity {
         try {
             Toolbar toolbar = findViewById(R.id.toolbar);
             setSupportActionBar(toolbar);
-            getSupportActionBar().setTitle("bars near"+venueName);
+            getSupportActionBar().setTitle("bars near "+venueName);
         } catch (NullPointerException e) {
             e.printStackTrace();
         }
+        accessBars=new AccessLocations();
+        try {
+            bars=accessBars.getBars(venueName);
+        } catch (InvalidNameException e) {
+            e.printStackTrace();
+        }
+        RecyclerView rv = findViewById(R.id.venuesRV);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+        adapter = new NearestBarAdapter(bars, listener);
+        rv.setAdapter(adapter);
     }
 }
