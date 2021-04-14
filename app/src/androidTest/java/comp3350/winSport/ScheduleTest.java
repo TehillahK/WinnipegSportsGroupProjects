23eb24809f73f07a967f3ce31d56987c1c600075
Charina Duenas <duenasc@myumanitoba.ca>
2021-04-14 03:54:00 -0500

    Add new acceptance test
    - added display verification for ScheduleTest
    - new test: StandingsTest

--- app/src/androidTest/java/comp3350/winSport/ScheduleTest.java
+++ app/src/androidTest/java/comp3350/winSport/ScheduleTest.java
@@ -1,8 +1,5 @@
 package comp3350.winSport;
 
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.filters.LargeTest;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -16,6 +13,8 @@ import comp3350.winSport.presentation.MainActivity;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
@@ -35,13 +34,19 @@ public class ScheduleTest {
     public void viewSchedule(){
         onView(withId(R.id.schedule_layout)).perform(click());
         onView(allOf(withId(R.id.teamName), withText("Winnipeg Jets"))).perform(click());
+        //verify that the schedule was displayed
+        onView(allOf(withId(R.id.schedule_rv))).check(matches(isDisplayed()));
     }
 
     @Test
     public void viewMultipleSchedules(){
         onView(withId(R.id.schedule_layout)).perform(click());
         onView(allOf(withId(R.id.teamName), withText("Winnipeg Jets"))).perform(click());
+        //verify that the schedule was displayed
+        onView(allOf(withId(R.id.schedule_rv))).check(matches(isDisplayed()));
         pressBack();
         onView(allOf(withId(R.id.teamName), withText("Edmonton Oilers"))).perform(click());
+        //verify that the schedule was displayed
+        onView(allOf(withId(R.id.schedule_rv))).check(matches(isDisplayed()));
     }
 }
